bda270d6b7e227f8f20d898ea4d4d099066588d1
Bruno Martins <bgcngm@gmail.com>
2019-06-18 21:39:11 +0800

    op3: Convert lights HAL into a native binderized HAL

--- device.mk
+++ device.mk
@@ -286,9 +286,7 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    lights.msm8996 \
-    android.hardware.light@2.0-impl \
-    android.hardware.light@2.0-service
+    android.hardware.light@2.0-service.oneplus3 \
 
 # Lineage hardware
 PRODUCT_PACKAGES += \
