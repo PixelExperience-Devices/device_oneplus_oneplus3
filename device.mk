6a9737cacbc572a9a1886a8f14ff7ccc20028f8b
LuK1337 <priv.luk@gmail.com>
2019-09-25 18:42:24 +0200

    op3: Convert remaining configstore settings to props

--- device.mk
+++ device.mk
@@ -469,7 +469,9 @@ PRODUCT_PACKAGES += \
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000
+    ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.max_virtual_display_dimension=4096
 
 # Telephony
 PRODUCT_PACKAGES += \
