bac15d0700e54cc6da45bf6a35416052a6224b86
Ethan Chen <intervigil@gmail.com>
2019-10-20 01:17:32 -0700

    op3: Remove dead RCS packages
    Change-Id: Ie2c03fa11cb08c7ab900d7b666981d592aba19c7

--- device.mk
+++ device.mk
@@ -455,13 +455,6 @@ PRODUCT_PACKAGES += \
     init.oneplus3.rc \
     ueventd.qcom.rc
 
-# RCS
-PRODUCT_PACKAGES += \
-    rcs_service_aidl \
-    rcs_service_aidl.xml \
-    rcs_service_api \
-    rcs_service_api.xml
-
 # Recovery
 PRODUCT_PACKAGES += \
     librecovery_updater_op3
