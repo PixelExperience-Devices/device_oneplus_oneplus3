5d95726980140ebe64ee19849b5cf4af77dece1d
ZJRDroid <priv.zjr@gmail.com>
2020-01-18 10:06:54 -0300

    op3: Display ARCore apps on Play Store

--- device.mk
+++ device.mk
@@ -398,7 +398,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
-    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
+    $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml \
+    $(LOCAL_PATH)/configs/android.hardware.camera.ar.xml:system/vendor/etc/permissions/android.hardware.camera.ar.xml
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.va_aosp.support=1
