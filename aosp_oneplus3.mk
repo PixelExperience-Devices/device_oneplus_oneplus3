0657f7575771dba006aeacba16501528c3fe14fd
Gaurav241 <gaurav.govinda2420@gmail.com>
2020-02-20 12:10:37 +0530

    op3: build ARcore

--- aosp_oneplus3.mk
+++ aosp_oneplus3.mk
@@ -39,3 +39,5 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 BUILD_FINGERPRINT := "OnePlus/OnePlus3/OnePlus3:9/PKQ1.181203.001/1907311932:user/release-keys"
 
 TARGET_VENDOR := oneplus
+
+TARGET_INCLUDE_STOCK_ARCORE := true
