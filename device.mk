2094171cc69b8c09916839cc08aec406996779c0
Gaurav241 <gaurav.govinda2420@gmail.com>
2020-02-23 00:31:42 +0000

    op3: Build live wallpaper package

--- device.mk
+++ device.mk
@@ -533,6 +533,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vr.high_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vr.high_performance.xml
 
+# Wallpapers
+PRODUCT_PACKAGES += \
+    PixelLiveWallpaperPrebuilt
+
 # Wifi
 PRODUCT_PACKAGES += \
     ipacm \
