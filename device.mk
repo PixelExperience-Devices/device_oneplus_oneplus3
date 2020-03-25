e990bf78f25f96fdecec665fe5e550ca4227ca77
Alexander Koskovich <zvnexus@outlook.com>
2020-03-25 12:45:24 -0400

    op3: Add hostapd configurations from LA.UM.8.6.2.r1-06600-89xx.0.
     * [THULIUM]
    
    Change-Id: I824a39e24e185ffd1adc2a0d02b9f6251ccf7faa

--- device.mk
+++ device.mk
@@ -554,6 +554,9 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/wifi/hostapd.accept:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.accept \
+    $(LOCAL_PATH)/wifi/hostapd.conf:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd_default.conf \
+    $(LOCAL_PATH)/wifi/hostapd.deny:$(TARGET_COPY_OUT_VENDOR)/etc/hostapd/hostapd.deny \
     $(LOCAL_PATH)/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
 
