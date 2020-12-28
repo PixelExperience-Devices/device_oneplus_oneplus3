a3a2f677e3bbb27525b95fce8c87f445eab50105
Kilian von Pflugk <github@jumoog.io>
2020-12-28 08:05:51 +0100

    op3: Remove TARGET_USES_64_BIT_BINDER
    The TARGET_USES_64_BIT_BINDER option is deprecated and can be removed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,8 +55,6 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := kryo
 
-TARGET_USES_64_BIT_BINDER := true
-
 # Kernel
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 cma=32M@0-0xffffffff loop.max_part=7
 #BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
