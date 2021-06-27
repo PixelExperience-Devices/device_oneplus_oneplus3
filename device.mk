8165940256b684c2ab4c94d4809213048dab5b27
Abhinavgupta371 <abhinavgupta371@gmail.com>
2021-06-27 13:08:32 +0000

    op3: Build vendor android.hidl.base@1.0.so
    meanwhile This reverts commit db010da.

--- device.mk
+++ device.mk
@@ -291,7 +291,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_ENFORCE_VINTF_MANIFEST_OVERRIDE := true
 
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_vendor \
+    android.hidl.manager@1.0
 
 # IRSC
 PRODUCT_COPY_FILES += \
