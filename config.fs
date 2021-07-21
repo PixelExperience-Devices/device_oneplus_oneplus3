b79f0cb85a5b9b4c7cef38afdf034d22de10dab5
IMMANUEL44 <immanuelr44@gmail.com>
2021-07-21 12:52:15 +0530

    op3: Update config.fs from LA.UM.9.6.3.r1-04200-89xx.0

--- config.fs
+++ config.fs
@@ -16,6 +16,15 @@ value:2905
 [AID_VENDOR_QRTR]
 value:2906
 
+[AID_VENDOR_THERMAL]
+value:2907
+
+[AID_VENDOR_FASTRPC]
+value:2908
+
+[AID_VENDOR_QTR]
+value:2909
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
@@ -35,6 +44,18 @@ group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
 [system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
+mode: 0755
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
+caps: BLOCK_SUSPEND NET_ADMIN
+
+[system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti-lazy]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -84,37 +105,43 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [system/vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/imsdaemon]
+mode: 0755
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
@@ -160,19 +187,7 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
-[system/vendor/bin/xtwifi-client]
-mode: 0755
-user:  AID_GPS
-group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
-
 [vendor/bin/sensors.qti]
-mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
-caps: NET_BIND_SERVICE
-
-[system/vendor/bin/sensors.qti]
 mode: 0755
 user: AID_SYSTEM
 group: AID_SYSTEM
@@ -193,12 +208,6 @@ caps: 0
 [vendor/firmware_mnt/image/*]
 mode: 0771
 user: AID_SYSTEM
-group: AID_SYSTEM
-caps: 0
-
-[system/vendor/firmware_mnt/image/*]
-mode: 0771
-user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 
