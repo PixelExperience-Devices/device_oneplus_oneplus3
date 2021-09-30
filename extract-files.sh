8090d4acd3e50724a24accf3aae5df0746c74fe3
Gaurav241 <gaurav.govinda2420@gmail.com>
2021-09-30 05:13:25 +0000

    op3: no more need of blob fixup

--- extract-files.sh
+++ extract-files.sh
@@ -53,14 +53,6 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
-function blob_fixup() {
-    case "${1}" in
-        etc/permissions/qti_libpermissions.xml)
-            sed -i "s/name=\"android.hidl.manager-V1.0-java/name=\"android.hidl.manager@1.0-java/g" "${2}"
-            ;;
-    esac
-}
-
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
