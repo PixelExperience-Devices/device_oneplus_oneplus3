3b3577e6aff7ea9fd8db734c4a8d2aad3763c4b7
Gaurav241 <gaurav.govinda2420@gmail.com>
2021-11-15 22:24:46 +0530

    op3: Uprev IMS manifest

--- device.mk
+++ device.mk
@@ -424,7 +424,7 @@ PRODUCT_PACKAGES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.4 \
-    android.hardware.radio.config@1.0 \
+    android.hardware.radio.config@1.1 \
     android.hardware.secure_element@1.0 \
     librmnetctl \
     libxml2 \
